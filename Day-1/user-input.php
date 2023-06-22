faa05862fcb8a4e69ef65a247dab70a5e3459c1a
Md. Arif Dewan <arif.dewan@oscillosoft.com.au>
2023-06-22 14:18:19 +0600

    sanitize the input

--- Day-1/user-input.php
+++ Day-1/user-input.php
@@ -1,7 +1,10 @@
 <?php
 
-$input1 = readline("Give your first input: ");
+$input1 = (int)readline("Give your first input: ");
 
-$input2 = readline("Give your second input: ");
-
-echo "Total is: ".($input1+$input2);
+$input2 = (int)readline("Give your second input: ");
+if(is_numeric(isset($input1)) && is_numeric(isset($input2))) {
+    echo "Total is: ".($input1+$input2);
+} else {
+    echo "You are not provided inputs correctly.";
+}
